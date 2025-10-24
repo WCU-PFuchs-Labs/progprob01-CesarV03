bca929d5e7c730effbf8d15b11ca1ec00f2ecd6e
CesarV03 <3021762@wcupa.edu>
2025-10-24 12:27:44 -0400

    homework20_1.java

--- homework20_1.java
+++ homework20_1.java
@@ -15,19 +15,41 @@ output: head-->1-->2-->3-->null
 
 Please complete the following program to fullfil the function.
 */
+
+// ran in jgrasp prior to execute expected output
 public class homework20_1
 {
    public static void main(String[] args)
    {
       //add your code here
+      LinkedList list = new LinkedList();
+      list.head = new ListNode(1);
+      list.head.next = new ListNode(2);
+      list.head.next.next = new ListNode(3);
+      list.head.next.next.next = new ListNode(3);
+      list.head.next.next.next.next = new ListNode(5);
+      deleteDuplicates(list);
+      System.out.println(list);
    
    }
    public static void deleteDuplicates(LinkedList llist)
    {
       //add your code here
+      ListNode current = llist.head;
+      while (current != null && current.next != null)
+      {
+         if (current.value == current.next.value)
+         {
+            current.next = current.next.next;
+         }
+         else
+         {
+            current = current.next;
+         }
+      }
    }
-
 }
+
 
 class ListNode
 {
